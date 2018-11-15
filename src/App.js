70ab24b95aa661ee92ac121e4d2c377a4ab2917d
loler <zaal@lider-bet.com>
2018-11-15 18:15:35 +0400

    Two way binding

--- src/App.js
+++ src/App.js
@@ -17,13 +17,20 @@ class App extends Component {
             {name: 'Bacho', age: 28}
         ]});
     }
+    changeAgeHandler = (event) => {
+        this.setState({persons: [
+            {name: 'Zaal', age: 25},
+            {name: 'Irakli', age: 25},
+            {name: 'Bacho', age: event.target.value}
+        ]});
+    }
 
     render() {
         return (
             <div className="App">
                 <Person name={this.state.persons[0]["name"]} age={this.state.persons[0]["age"]}/>
                 <Person name={this.state.persons[1]["name"]} age={this.state.persons[1]["age"]}> I love Front-End Development! </Person>
-                <Person click={this.switchNameHandler} name={this.state.persons[2]["name"]} age={this.state.persons[2]["age"]}/>
+                <Person changed={this.changeAgeHandler} click={this.switchNameHandler} name={this.state.persons[2]["name"]} age={this.state.persons[2]["age"]}/>
                 <button onClick={this.switchNameHandler}>Switch</button>
             </div>
         );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old! {props.children}</p>
+    return (<p onClick={props.click}>I'm {props.name} and I'm {props.age} years old! {props.children} <input type = "text" onChange={props.changed} value={props.age}/></p> )
 }
 
 export default person;
