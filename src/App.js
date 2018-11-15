ce267cf815523ee8dd30b2a9d0da30f2286c75f8
loler <zaal@lider-bet.com>
2018-11-15 18:29:33 +0400

    Added CSS Added inline CSS

--- src/App.js
+++ src/App.js
@@ -23,15 +23,24 @@ class App extends Component {
             {name: 'Irakli', age: 25},
             {name: 'Bacho', age: event.target.value}
         ]});
-    }
+    };
+
 
     render() {
+        const inlineStyleForButton = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid green',
+            padding: '8px',
+            cursor: 'pointer'
+
+        }
         return (
             <div className="App">
                 <Person name={this.state.persons[0]["name"]} age={this.state.persons[0]["age"]}/>
                 <Person name={this.state.persons[1]["name"]} age={this.state.persons[1]["age"]}> I love Front-End Development! </Person>
                 <Person changed={this.changeAgeHandler} click={this.switchNameHandler} name={this.state.persons[2]["name"]} age={this.state.persons[2]["age"]}/>
-                <button onClick={this.switchNameHandler}>Switch</button>
+                <button style={inlineStyleForButton} onClick={this.switchNameHandler}>Switch</button>
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import './Person.css'
 const person = (props) => {
-    return (<p onClick={props.click}>I'm {props.name} and I'm {props.age} years old! {props.children} <input type = "text" onChange={props.changed} value={props.age}/></p> )
+    return (<p className="Person" onClick={props.click}>I'm {props.name} and I'm {props.age} years old! {props.children} <input type = "text" onChange={props.changed} value={props.age}/></p> )
 }
 
 export default person;
